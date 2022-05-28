3feeba3156b778f65eeda1635b7798950ba6ec87
Sortren <sortren.dev@gmail.com>
2022-05-29 00:33:03 +0200

    add CurrentUserProfileData endpoint

--- src/api/controllers/spotify_controller.py
+++ src/api/controllers/spotify_controller.py
@@ -7,9 +7,21 @@ from misc.config import Config
 spotify_controller = Blueprint("spotify_controller", __name__)
 
 
+class CurrentUserProfileData(MethodView):
+    def get(self):
+        bearer_token = request.headers.get("Authorization")
+
+        profile_data = req.get(
+            "https://api.spotify.com/v1/me",
+            headers={"Authorization": bearer_token}
+        )
+
+        return make_response(profile_data, 200)
+
+
 class Login(MethodView):
     def get(self):
-        scopes = 'user-library-read'
+        scopes = 'playlist-modify-public playlist-modify-private'
 
         url = req.Request('GET', "https://accounts.spotify.com/authorize", params={
             'scope': scopes,
@@ -25,7 +37,7 @@ class AuthorizeCallback(MethodView):
     def get(self):
         code = request.args.get("code")
 
-        response = req.post("https://accounts.spotify.com/api/token", data={
+        access_data = req.post("https://accounts.spotify.com/api/token", data={
             'grant_type': 'authorization_code',
             'code': code,
             'redirect_uri': Config.SPOTIFY_REDIRECT_URI,
@@ -33,7 +45,7 @@ class AuthorizeCallback(MethodView):
             'client_secret': Config.SPOTIFY_CLIENT_SECRET
         }).json()
 
-        return make_response(response, 200)
+        return make_response(access_data, 200)
 
 
 class TemporarySpotifyRedirectEndpoint(MethodView):
@@ -47,6 +59,9 @@ spotify_controller.add_url_rule(
 
 spotify_controller.add_url_rule(
     "/authorizeCallback", view_func=AuthorizeCallback.as_view("authorizeCallback"))
+
+spotify_controller.add_url_rule(
+    "/currentUserProfileData", view_func=CurrentUserProfileData.as_view("currentUserProfileData"))
 
 spotify_controller.add_url_rule(
     "/temporary", view_func=TemporarySpotifyRedirectEndpoint.as_view("temporary"))
